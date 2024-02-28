ef110694ef6c96581d58dba2203b7e11b3c07514
Tsion Moges <tsionmoges25@gmail.com>
2024-02-28 22:33:13 +0300

    adding updates

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,11 +1,183 @@
-import React from 'react'
+import React from "react";
+import Logo from "../Images/logo.png";
 
 const Footer = () => {
   return (
     <div>
-      
+      <div className="border-t border-gray-300 mt-20">
+        <div className="flex items-center gap-10 mt-5">
+          <div className="w-1/4 flex flex-col items-center px-4">
+            <img src={Logo} alt="Logo" className="w-20" />
+            <p className="font-bold text-sm text-gray-500 mt-4">
+              ይህ የደብረ መድኃኒት መድኃኔዓለም ቤተክርስቲያን ድህረገጽ ነው። ላላወቁት በማሳወቅ የድርሻችንን እንወጣ።
+            </p>
+            <div className="flex gap-4 items-center mt-6">
+            <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z"
+                  />
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z"
+                  />
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z"
+                  />
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z"
+                  />
+                </svg>
+            </div>
+          </div>
+          <div className="w-3/4 grid grid-cols-3 gap-4">
+            <div className="flex flex-col">
+              <h4 className="font-bold text-lg ">እኛን ለማግኘት</h4>
+              <div className="flex mt-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z"
+                  />
+                </svg>
+                <p className="font-bold text-sm text-gray-500 ml-2">
+                  +251-9001-12233
+                </p>
+              </div>
+              <div className="flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z"
+                  />
+                </svg>
+                <p className="font-bold text-sm text-gray-500 ml-2">
+                  debremedhanit27@gmail.com
+                </p>
+              </div>
+              <div className="flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z"
+                  />
+                </svg>
+                <p className="font-bold text-sm text-gray-500 ml-2">
+                  ቦሌ ቡልቡላ፣ አዲስ አበባ ኢትዮጵያ
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-col">
+              <h4 className="font-bold text-lg ">ሌሎች ገጾች</h4>
+              <div className="mt-4">
+                <ul className="font-bold text-sm text-gray-500">
+                  <li className="">
+                    <a href="#">ዋና ገጽ</a>
+                  </li>
+                  <li>
+                    <a href="#">ዋና ገጽ</a>
+                  </li>
+                  <li>
+                    <a href="#">ዋና ገጽ</a>
+                  </li>
+                  <li>
+                    <a href="#">ዋና ገጽ</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="flex flex-col">
+              <h4 className="font-bold text-lg ">ስለ ደብራችን</h4>
+              <div className="mt-4">
+                <ul className="font-bold text-sm text-gray-500">
+                  <li className="">
+                    <a href="#">እርዳታ</a>
+                  </li>
+                  <li>
+                    <a href="#">ሰንበት ትምህርት ቤት</a>
+                  </li>
+                  <li>
+                    <a href="#">መርሃግብራት</a>
+                  </li>
+                  <li>
+                    <a href="#">ዋና ገጽ</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>    
+      </div>
+      <div className="border-t border-gray-300 mt-10 py-4">
+    <p className="text-gray-500 text-xs">copyright @ 2024 debremedhanit. All rights reserved</p>
+</div>
+{/* <div className="mt-10"></div> */}
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;

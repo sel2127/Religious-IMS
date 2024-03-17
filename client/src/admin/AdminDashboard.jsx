6490407b1690beb0e92a1e3716b1d4af6b7c794b
Tsion Moges <tsionmoges25@gmail.com>
2024-03-17 13:26:36 +0300

    updates

---
+++ client/src/admin/AdminDashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { ColorModeContext, useMode } from './theme';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+
+function AdminDashboard() {
+  return (
+    <div className='dashboard flex relative'>
+      
+    </div>
+  )
+}
+
+export default AdminDashboard

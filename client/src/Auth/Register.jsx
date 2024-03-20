971e80d3d6d432e589625fbefc3235cefc53316a
tseloteyonas <nomiyonas21@gmail.com>
2024-03-20 06:31:30 -0700

    new

--- client/src/Auth/Register.jsx
+++ client/src/Auth/Register.jsx
@@ -3,15 +3,25 @@ import '../assets/styles/main.css';
 import Breadcrumb from '../common/Breadcrumb';
 
 const Register = () => {
+
+  const [firstnameReg, setFirstnameReg] =useState("");
+  const [lastnameReg, setLastnameReg] =useState("");
+  const [emailReg, setEmailReg] =useState("");
+  const [passwordReg, setPasswordReg] =useState("");
   return (
     <div>
 
       <div className='mx-auto border border-gray-300 w-1/2 mt-10 rounded rounded-3xl text-gray-600'>
         <div className='flex flex-col items-center justify-center px-20 py-10'>
     
-        <input type="text" placeholder='ስም' className=' mt-10 w-full h-10 px-6 border border-gray-300  rounded-full' />
-        <input type="text" placeholder='ኢሜል' className=' mt-6 w-full h-10 px-6 border border-gray-300  rounded-full' />
-        <input type="text" placeholder='ይለፍ ቃል' className=' mt-6 w-full h-10 px-6 border border-gray-300  rounded-full' />
+        <input type="text" onChange = {(e) => { setFirstnameReg(e.target.value);}}
+         placeholder='የመጀመሪያ ስም' className=' mt-10 w-full h-10 px-6 border border-gray-300  rounded-full' />
+        <input type="text" onChange = {(e) => { setLastnameReg(e.target.value);}}
+        placeholder='የአባት ስም' className=' mt-10 w-full h-10 px-6 border border-gray-300  rounded-full' />
+        <input type="text" onChange = {(e) => { setEmailReg(e.target.value);}}
+        placeholder='ኢሜል' className=' mt-6 w-full h-10 px-6 border border-gray-300  rounded-full' />
+        <input type="text" onChange = {(e) => { setPasswordReg(e.target.value);}}
+        placeholder='ይለፍ ቃል' className=' mt-6 w-full h-10 px-6 border border-gray-300  rounded-full' />
         <input type="text" placeholder='ይለፍ ቃል አረጋግጥ' className=' mt-6 w-full h-10 px-6 border border-gray-300  rounded-full' />
         <div className='mr-auto underline decoration-dotted mt-4 cursor-pointer hover:text-[#79a6d2]'>
             <a href="/login">አካውንት አለዎት?</a></div>            

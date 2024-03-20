e71a3df7287c68ab926aeddb8982214238355042
Selina Yeshanew <88796374+seli1212@users.noreply.github.com>
2024-03-20 16:28:59 +0300

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -11,18 +11,11 @@ import Header from './common/Header'
 import Footer from './common/Footer'
 import FeedbackPage from './pages/FeedbackPage';
 import AbnetPage from './pages/AbnetPage';
-import Upload from './admin/EventUpload';
+import Upload from './components/EventUpload';
 import Donation from './pages/Donation';
 import MemberRegisterationPage from './pages/MemberRegisterationPage';
 import Notify from './pages/Notify';
-import Login from './pages/Login'
-import Register from './pages/Register'
-import Forgot from './pages/Forgot'
-import Notify from './pages/Notify';
-import Header from './components/Header'
-import Breadcrumb from './components/Breadcrumb';
-import Footer from './components/Footer'
-import EventUpload from './admin/EventUploadPage'
+import EventUpload from './components/EventUpload'
 
 
 
@@ -30,7 +23,6 @@ function App() {
   return (
     <Router>
       <Header/>
-      <Breadcrumb/>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/church" element={<ChurchPage />} />
@@ -39,7 +31,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/forgot" element={<Forgot />} />
         <Route path="/notify" element={<Notify />} />
-        <Route path="/eventupload" element={<EventUpload />} />
+        <Route path="/upload" element={<EventUpload />} />
       </Routes>
       <Footer/>
     </Router>
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './store/store';
 import './index.css';
 import App from './App'; 
 
-createRoot(document.getElementById('root')).render(
-        <App />
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
 );

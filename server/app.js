36165493a5e9743d0ed92328a8d0411ca9b58bcb
Selina Yeshanew <88796374+seli1212@users.noreply.github.com>
2024-03-25 23:13:54 +0300

    commit

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const multer  = require('multer');
 const eventRoutes = require('./routes/eventRoutes');
+const { Chapa } = require('chapa');
+
 
 const app = express();
 

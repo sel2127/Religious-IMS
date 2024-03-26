e15454ea0bda7ef1ec52e87293fa39021645d3ba
Tsion Moges <tsionmoges25@gmail.com>
2024-03-26 13:40:29 +0300

    updates

--- client/src/App.js
+++ client/src/App.js
@@ -11,19 +11,12 @@ import Breadcrumb from './common/Breadcrumb';
 import Footer from './common/Footer'
 import FeedbackPage from './pages/FeedbackPage';
 import AbnetPage from './pages/AbnetPage';
-<<<<<<< HEAD
 import Upload from './components/EventUpload';
 import Donation from './pages/Donation';
 import MemberRegisterationPage from './pages/MemberRegisterationPage';
 import Notify from './pages/Notify';
 import EventUpload from './components/EventUpload';
-=======
-import FeedbackForm from './components/FeedbackForm';
-import ProfilePage from './pages/ProfilePage';
-import EditProfile from './components/profile/EditProfile';
-import ChangePassowrd from './components/profile/ChangePassword';
->>>>>>> origin/maste
-
+import Admin from './admin/Admin'
 
 
 function App() {
@@ -37,22 +30,12 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/forgot" element={<Forgot />} />
-<<<<<<< HEAD
         <Route path="/notify" element={<Notify />} />
         <Route path="/upload" element={<EventUpload />} />
-=======
-        <Route path='/feedback' element={<FeedbackPage/>}/>
-        <Route path='/abnet' element={<AbnetPage/>}/>
-        <Route path='/feedbackform' element={<FeedbackForm/>}/>
-        <Route path='/profile' element={<ProfilePage/>}/>
-        <Route path='/editprofile' element={<EditProfile/>}/>
-        <Route path='/changepassword' element={<ChangePassowrd/>}/>
-
-
->>>>>>> origin/maste
+        <Route path="/admin/*" element={<Admin />} />
 
       </Routes>
-      {/* <Footer/> */}
+      <Footer/>
     </Router>
   );
 }

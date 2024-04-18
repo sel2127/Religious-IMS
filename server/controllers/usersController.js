866afbc01eba56a2ea41c6ceef9f18ada4b4bdc2
Tsion Moges <tsionmoges25@gmail.com>
2024-04-18 08:47:22 +0300

    updates

---
+++ server/controllers/usersController.js
@@ -0,0 +1,17 @@
+import UserModel from "../models/Users.js";
+
+export const getUsers = (req, res) => {
+    
+}
+export const getUserById = (req, res) => {
+    
+}
+export const createUser = (req, res) => {
+    
+}
+export const updateUser = (req, res) => {
+    
+}
+export const deleteUser = (req, res) => {
+    
+}

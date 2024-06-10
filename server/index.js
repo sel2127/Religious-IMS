fb72054566890d1f4b4ebe2a6056a3334288b6c2
Tsion Moges <tsionmoges25@gmail.com>
2024-06-10 22:17:19 +0300

    updates

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
-import db from './config/Database.js';
+import db from './config/Database.js'
 import eventRouter from './routes/eventRoutes.js';
 import uploadRouter from './routes/uploadRoutes.js';
 import adminRouter from './routes/adminRoutes.js';
